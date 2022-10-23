1f91a57d93ac676eb513ea0c0cfe06521f522f9c
hyunjo325 <115158679+hyunjo325@users.noreply.github.com>
2022-10-24 01:43:40 +0900

    Update miniMarble_final.py

--- miniMarble_final.py
+++ miniMarble_final.py
@@ -502,6 +502,7 @@ def judge_own_city_name(city_name):
         for i in range(20):
             if city_name == default_map_name[i]:
                 if owner_list[i] != id_list[now_order]: 
+                    print('===== ' + default_map_name[] + ' 도시를 보유하고 있지 않습니다! =====')
                     return False
                 else:
                     return i
